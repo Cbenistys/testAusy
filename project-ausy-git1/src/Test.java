220d78f4a9ce195d44aa97376624e0db6fa6cd93
Celia BENISTY <celia.benistys@gmail.com>
2021-06-03 13:57:18 +0200

    Version 2

--- project-ausy-git1/src/Test.java
+++ project-ausy-git1/src/Test.java
@@ -3,7 +3,7 @@ public abstract class Test {
 
 	public static void main(String[] args) {
 		System.out.println("projet ausy git 1 version 1");
-
+		System.out.println("projet ausy git 1 version 2");
 	}
 
 }
